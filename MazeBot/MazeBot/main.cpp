6370f05a40475bc2e6fcfd00935d7b259d85b788
Ceyhan Yildiz <12200089@student.pxl.be>
2023-12-02 04:36:09 +0100

    Clean main

--- MazeBot/MazeBot/main.cpp
+++ MazeBot/MazeBot/main.cpp
@@ -1,34 +1,8 @@
-// Sfml
-#include <SFML/Graphics.hpp> // Window app
-
-// C++
-#include <iostream> //Standard Input/Output Streams
-#include <vector> // Dynamic Array
-#include <string> //String Class 
-
-#include <ctime>   // Time Functions 
-#include <random> // Include for modern random number generation
-
-// Cell Element
-#include "Checkpoint.h"
-#include "End.h" 
-#include "MazeElement.h"    
-#include "OutOfBounds.h"
-#include "Path.h"
-#include "Start.h"
-#include "Wall.h"   
-#include "WrongPath.h"      
-
-// Main Game
 #include "Maze.h"
-#include "TestGame.h"
 
-// shortCuts
-using namespace sf;
-using namespace std;
 
 int main() {
-    Maze game(1440,740,20);
+    Maze game(1440, 740, 15); // Sweet Spot 1440, 740, 15
     game.run();  // WHILE LOOP !!
 
     return 0;
