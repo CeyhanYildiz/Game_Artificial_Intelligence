f346e48265f3c7e350fadfb2060b0493260b7557
Ceyhan <ceyhanyildizt3@gmail.com>
2023-12-11 22:59:57 +0100

    thread workings

--- MazeBot/MazeBot/main.cpp
+++ MazeBot/MazeBot/main.cpp
@@ -1,19 +1,32 @@
-// Class
+#include <iostream>
+#include <thread>
+#include <vector>
 #include "Maze.h"
 
-// namespace
 using namespace std;
 
 int main() {
+    const int numberOfGames = 2;
+    vector<thread> gameThreads;
 
-    // Make game
-        Maze game(10,29);
+    for (int i = 1; i <= numberOfGames; ++i) {
+        // Create a thread to make the gam
+        gameThreads.emplace_back([i]() {
+            // Maze object creation and any additional setup
+            Maze game(10, 29);
 
-    // Make AI
-        //Here
+            // You can use i to differentiate between threads if needed
+            cout << "Thread " << i << " started.\n";
 
-    // Running 
-        game.run();  // game.run(); = Its a loop FIX!
+            game.run();
+            });
+        this_thread::sleep_for(chrono::seconds(4));
+    }
+
+    // Join all the game threads after the loop
+    for (auto& thread : gameThreads) {
+        thread.join();
+    }
 
     return 0;
 }
